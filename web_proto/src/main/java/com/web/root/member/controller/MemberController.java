591ad270ea2e2b1fc341ddb9f6f3679acc5a1854
juho <orot1195@gmail.com>
2023-05-31 22:33:25 +0900

    fff

--- web_proto/src/main/java/com/web/root/member/controller/MemberController.java
+++ web_proto/src/main/java/com/web/root/member/controller/MemberController.java
@@ -42,6 +42,17 @@ public class MemberController {
 	private String test4;
 	private String test10;
 	private String test11;
+//	private String test5;
+//	private String test6;
+//	private String test7;
+//	private String test8;
+//	private String test9;
+//	private String test1;
+//	private String test2;
+//	private String test3;
+//	private String test4;
+//	private String test10;
+//	private String test11;
 	
 
 }
